72259158f0bcd9c646aa7cdb88dfbfa42c54ee65
ALLOUDH <hectorgz.hcp@gmail.com>
2022-04-18 16:27:40 -0500

    Multiplo de 15 Implementado V3.0

--- test/TingoTangoTest.py
+++ test/TingoTangoTest.py
@@ -6,7 +6,7 @@ class TingoTangoPrueba(unittest.TestCase):
         self.TT = TingoTango()
     def tearDown(self):
         self.TT = TingoTango()
-    def test_TingoTango_MultipleTres_retornaTingo(self):
+    def test_TingoTango_MultiploTres_retornaTingo(self):
         # Arranque
             self.numero1 = 3
             self.resultadoEsperado = "Tingo"
@@ -15,10 +15,19 @@ class TingoTangoPrueba(unittest.TestCase):
         # Assert
             self.assertEqual(self.resultadoEsperado,self.resultadoActual)
 
-    def test_TingoTango_MultipleCinco_retornaTango(self):
+    def test_TingoTango_MultiploCinco_retornaTango(self):
         # Arranque
             self.numero1 = 5
             self.resultadoEsperado = "Tango"
+        # Do
+            self.resultadoActual = self.TT.textoTingoTango(self.numero1)
+        # Assert
+            self.assertEqual(self.resultadoEsperado,self.resultadoActual)
+
+    def test_TingoTango_MultiploQuince_retornaTingoTango(self):
+        # Arranque
+            self.numero1 = 15
+            self.resultadoEsperado = "TingoTango"
         # Do
             self.resultadoActual = self.TT.textoTingoTango(self.numero1)
         # Assert
